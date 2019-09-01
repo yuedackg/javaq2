a87d26e3d1249054e1f0a9e01a41a4f4a2d33bb7
yoshihiro UEDA <y.ueda@ckg.ac.jp>
2019-09-01 14:54:03 +0900

    14:53 error included.

--- src/Editor.java
+++ src/Editor.java
@@ -1,4 +1,48 @@
 
 public class Editor {
+	private GapBuffer buf;
+	private int cursor = 0;
+	
+	private Editor( String text) {
+		buf = new GapBuffer( text);
+	}
+	private void run() {
 
+		//	Dispalay.output( buf, cursor);
+		char ch;
+		while((ch=CharReader.get()) != CharReader.EOF) {
+			switch( ch) {
+			case CharReader.MOVE_FORWARD:
+				moveCursor( 1);
+				break;
+			case CharReader.MOVE_BACKWARD:
+				moveCursor( -1);
+				break;
+			case CharReader.DELETE:
+				if ( cursor <buf.length()) {
+					buf.delete();
+				}
+			default:
+				buf.insert(,ch);
+				break;
+			}
+			//	Display.output( buf,cursor);
+			
+		}
+	}
+}
+
+class CharReader{
+	static final int MOVE_FORWARD = 1;
+	static final int MOVE_BACKWARD = 2;
+	static final int DELETE = 3;
+	static final int INSERT = 3;
+	public   char get() {
+		return 0;
+		
+	}
+	public void delete() {
+		
+	}
+	
 }
--- src/GapBuffer.java
+++ src/GapBuffer.java
@@ -1,4 +1,32 @@
 
 public class GapBuffer {
-
+	private static final int INITIAL_GAP_SIZE = 128;
+	private char[] buffer;
+	private int gapOffset = 0;
+	private int gapSize = INITIAL_GAP_SIZE;
+	
+	GapBuffer(String initialText) {
+		buffer = new char[ initialText.length() + gapSize];
+		System.arraycopy(initialText.toCharArray(), 0, buffer, gapSize, initialText.length());
+		
+	}
+	
+	void insert( int offset, char ch) {
+		confirmGap( offset);
+		buffer[gapOffset++]=ch;
+		
+	}
+	void delete( int offset) {
+		
+	}
+	char charAt( int offset) {
+		
+	}
+	int length( ) {
+		return 	;
+	
+	}
+	private void confirmGap( int newGapOffset) {
+		
+	}
 }
